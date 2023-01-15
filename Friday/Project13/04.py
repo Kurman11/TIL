da9b57d4f757c7abe2cda7e41d3a8fc48e78c9b9
Kurman11 <skedyd45@naver.com>
2023-01-15 23:21:07 +0900

    Update15

--- Friday/Project13/04.py
+++ Friday/Project13/04.py
@@ -3,7 +3,39 @@ from pprint import pprint
 
 
 def search_movie(title):
-    pass
+# https://api.themoviedb.org/3/movie/popular?api_key=0b62813d9d8d3c1c77f760c5d3ef3f52
+    BASE_URL = 'https://api.themoviedb.org/3'
+    path = '/search/movie'
+    params = {
+        'api_key' : '0b62813d9d8d3c1c77f760c5d3ef3f52',
+        'language' : 'ko-KR',
+        'query' : title,
+        'region' : 'KR'
+    }
+    response = requests.get(BASE_URL + path, params = params).json()
+
+    
+    # total_results = response.get('total_results')
+    # if total_results != 0 :      
+    #     id = response.get('results')[0].get('id')
+    #     return id
+    # elif total_results == 0:
+    #     return 'None'
+
+
+    try:
+        id = response.get('results')[0].get('id')
+        title = response.get('results')[0].get('title')
+        if '기생충' in title:
+            return id
+        elif '그래비티' in title:
+            return id
+    except:
+        return 'None'
+
+
+
+
     # 여기에 코드를 작성합니다.
 
 
