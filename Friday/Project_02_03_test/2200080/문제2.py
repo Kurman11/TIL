aa07b89b10ae217cb765db7b7fc7472d6a081ec1
Kurman11 <skedyd45@naver.com>
2023-02-05 23:27:35 +0900

    Update_02_05

--- Friday/Project_02_03_test/2200080/문제2.py
+++ Friday/Project_02_03_test/2200080/문제2.py
@@ -4,21 +4,22 @@ import sys,pprint
 sys.stdin = open('input.txt','r')
 
 T = int(input())
-a = []
-b = []
 for t in range(1,T+1):
     N, M = list(map(int,input().split()))
     num = [list(map(int,input().split())) for _ in range(N)]
     num1 = [[0 for _ in range(M)] for _ in  range(M)]
     n = M-1
-    for i in range(N-n):
-        for x in range(N-n):
-            print(num[i][x])
-# pprint.pprint(num)
-# pprint.pprint(num1)
-    # for i in range(N):
-    #     num = list(map(int,input().split()))
-    #     for x in range(M):
-    #         num1 = list(map(int,input().split()))
-    #     print(num)
-        
+    t = 0
+    
+    for i in range(N):
+        a = 0
+        for x in range(M):
+            a += num[i][x+t]
+            if (x+t) != N-1:
+                t += 1
+            else:
+                break
+                
+        #     print(num[i][x-1])
+        # print()             
+        print(a)
