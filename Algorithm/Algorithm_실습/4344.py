7cce1373a385446327dffa88c2aa0914e9bec3cf
Kurman11 <skedyd45@naver.com>
2023-03-17 23:44:49 +0900

    Update_03_17

---
+++ Algorithm/Algorithm_실습/4344.py
@@ -0,0 +1,19 @@
+# 평균은 넘겠지
+
+import sys
+sys.stdin = open('input.txt','r')
+
+T = int(input())
+
+for t in range(T):
+    num = list(map(int,input().split()))
+    for i in range(num[0]):
+        cnt = 0
+        average = sum(num[1:])/num[0]
+        for x in num[1:]:
+            if average < x:
+                cnt += 1
+            a = (cnt / num[0])*100
+
+
+    print(f'{a:.3f}%')
