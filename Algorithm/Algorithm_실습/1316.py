c38a5f1a660d74c7ccbfbdee161ee62d96a95d7b
Kurman11 <skedyd45@naver.com>
2023-03-18 23:19:27 +0900

    Update_03_18

---
+++ Algorithm/Algorithm_실습/1316.py
@@ -0,0 +1,19 @@
+# 그룹 단어 체커
+
+import sys
+sys.stdin = open('input.txt','r')
+
+T = int(input())
+cnt = T
+for t in range(T):
+    word = input()
+    for i in range(0,len(word)-1):
+        if word[i] == word[i+1] :
+            pass
+        elif word[i] in word[i+1:] :
+            cnt -= 1
+            break
+print(cnt)
+        
+                
+
