2eae02611fc55de9d552f03fe2b90a635668eb32
Kurman11 <skedyd45@naver.com>
2023-03-26 09:29:15 +0900

    Update_03_26

--- Algorithm/Algorithm_실습/2563.py
+++ Algorithm/Algorithm_실습/2563.py
@@ -3,3 +3,17 @@
 import sys
 sys.stdin = open('input.txt','r')
 
+arr = [[0 for _ in range(100)] for _ in range(100)]
+
+T = int(input())
+a = []
+for t in range(T):
+    X,Y = map(int,input().split())
+    for x in range(X,X+10):
+        for y in range(Y,Y+10):
+            arr[x][y] = 1
+
+for x in range(100):
+    a.append(sum(arr[x]))
+
+print(sum(a))
