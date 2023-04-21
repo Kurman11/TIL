026f8a7c18c94b431772a49402b5b243560cd1ee
Kurman11 <skedyd45@naver.com>
2023-04-21 23:46:04 +0900

    Update_04_21

---
+++ Algorithm-study/Day/Swea/9700.py
@@ -0,0 +1,12 @@
+# USB 꽃기의 미스터리
+import sys
+sys.stdin = open('input.txt','r')
+
+T = int(input())
+
+for t in range(1, T+1):
+    p , q = map(float,input().split())
+    s1 = (1 - p) * q
+    s2 = p * (1 - q) * q
+
+    print(f'#{t}', "YES" if s2 > s1 else "NO")
